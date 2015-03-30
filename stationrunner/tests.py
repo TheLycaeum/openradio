efba160ce2433ec989ec8b02676cf95ebea1a1e7
Afzal Shahul Hameed <aflibnush@gmail.com>
2015-03-30 20:53:01 +0530

    Adds test for checking if a page exists at defined url pattern for creation of a station

--- stationrunner/tests.py
+++ stationrunner/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+class TestStationCreate(TestCase):
+    def test_page_exists(self):
+        """
+        Tests if a page exists for creation of station
+        """
+        response = self.client.get(reverse("createstation"))
+        self.assertEqual(response.status_code, 200)
+        
+        
+        
