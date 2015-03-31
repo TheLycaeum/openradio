f7ca149772d221417cd61b6eaeaacc65f0b39fa5
Afzal Shahul Hameed <aflibnush@gmail.com>
2015-03-31 19:52:23 +0530

    Changes tests to use simple assert commands instead of different django TestCase assert methods

--- stationrunner/tests.py
+++ stationrunner/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
-# Create your tests here.
 
 class TestStationCreate(TestCase):
     def test_page_exists(self):
@@ -9,16 +8,16 @@ class TestStationCreate(TestCase):
         Tests if a page exists for creation of station
         """
         response = self.client.get(reverse("createstation"))
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
     def test_contains_required_fields(self):
         """
         Tests if the page for creation of a station contains a form and
         the required fields
         """
         response = self.client.get(reverse("createstation"))
-        self.assertIn("form", response.context)
-        self.assertIn("name", response.content)
-        self.assertIn("address", response.content)
+        assert "form" in response.context
+        assert "name" in response.content
+        assert "address" in response.content
     def test_submission_form_creates_station_object(self):
         """
         Tests if submission of the form on page for creation of a 
@@ -30,10 +29,10 @@ class TestStationCreate(TestCase):
         response = self.client.post(reverse("createstation"),
                                     {"name": name, "address": address},
                                     follow=True)
-        self.assertIn("station", response.context)
+        assert "station" in response.context
         s = response.context["station"]
-        s.name = name
-        s.address = address
+        assert s.name == name
+        assert s.address == address
 
         
         
