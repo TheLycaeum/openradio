e2c27b1afef92e29cd6989a68ee2f99a16c82461
Afzal Shahul Hameed <aflibnush@gmail.com>
2015-04-16 09:31:57 +0530

    Adds owner field to Station model

--- stationrunner/models.py
+++ stationrunner/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 class Station(models.Model):
     name = models.CharField(max_length=50)
     address = models.TextField(default='')
+    owner = models.ForeignKey(User,default='')
     
     def get_absolute_url(self):
         return reverse("viewstation", kwargs={"pk": self.pk})
