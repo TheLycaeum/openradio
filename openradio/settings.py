3295cb335c6df1e5908ef9e1928b631610aa17dc
Afzal Shahul Hameed <aflibnush@gmail.com>
2015-05-27 12:05:46 +0530

    Adds middleware for redirecting post requests to pull/delete

--- openradio/settings.py
+++ openradio/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'stationrunner.middleware.HttpPostTunnelingMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
