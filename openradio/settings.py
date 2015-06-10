32c476735765574c6f99b4ba2100119647fdba80
Afzal Shahul Hameed <aflibnush@gmail.com>
2015-06-10 22:28:58 +0530

    Adds some common style

--- openradio/settings.py
+++ openradio/settings.py
@@ -105,10 +105,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-
 TEMPLATE_DIRS = (
     join(BASE_DIR,  'templates'),
+)
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    join(BASE_DIR, 'static'),
 )
 
 ABSOLUTE_URL_OVERRIDES = {
